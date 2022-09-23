53a119aebbcf9ae4a1e003dcda34121296fef750
e-hengirmen <ehengirmen@hotmail.com>
2022-09-24 01:13:08 +0300

    added libs

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("starting out")
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+print("check")
