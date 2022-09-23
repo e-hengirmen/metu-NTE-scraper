7427098f2915c083e76fedf61f17e5fa23b82f61
e-hengirmen <ehengirmen@hotmail.com>
2022-09-24 01:38:36 +0300

    NTE course codes scraped from webpage

--- main.py
+++ main.py
@@ -7,6 +7,40 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
+start = time.time()
+
+# mld always creates problems for everyone
+def mld_switch(str):
+    dict={
+        "ARAB": "602",
+        "FREN": "603",
+        "GERM": "604",
+        "JA": "605",
+        "ITAL": "606",
+        "RUS": "607",
+        "SPAN": "608",
+        "HEB": "609",
+        "GRE": "610",
+        "GREEK": "610",
+        "CHN": "611",
+        "PERS": "612",
+        "ENG": "639"
+    }
+    courseName=""
+    for c in str:
+        if c==' ' or c.isdigit():
+            break
+        courseName+=c
+    return dict[courseName]
+
+def write_num(str):
+    courseCode = ""
+    for c in str:
+        if c.isdigit():
+            courseCode += c
+    return courseCode
+
+
 NTE_URL = "https://muhfd.metu.edu.tr/en/nte-courses"
 
 url1 = "https://sis.metu.edu.tr/get.php?package="
@@ -22,4 +56,35 @@ driver = webdriver.Chrome()
 driver.maximize_window()
 action = ActionChains(driver)
 
-print("check")
+
+
+#################################
+driver.get(NTE_URL)
+rows = driver.find_elements(By.XPATH,'//*[@id="content"]/article/div[2]/table/tbody/tr')
+for i in rows:
+    cols=i.find_elements(By.TAG_NAME,'td')
+    # getting link and course_code
+    class_code=write_num(cols[0].text)
+    if (int(class_code)>=603 and int(class_code)<=639):
+        continue
+    i.click()
+
+    if(int(class_code)!=602):
+        course_table=WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,'//*[@id="content"]/article/div[2]/table/tbody/tr')))[1:]
+        for course in course_table:
+            course_code = write_num(course.find_elements(By.TAG_NAME, 'td')[0].text)
+            full_code = class_code + ("0" if len(course_code) == 3 else "") + course_code
+            course_set.add(full_code)
+    else:           # mld specific case their xpath and table structure are different
+        course_table = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="content"]/div[2]/table/tbody/tr')))[1:]
+        for course in course_table:
+            course_name=course.find_elements(By.TAG_NAME, 'td')[0].text
+            course_code = write_num(course_name)
+            class_code = mld_switch(course_name)
+            full_code = class_code + ("0" if len(course_code) == 3 else "") + course_code
+            course_set.add(full_code)
+    driver.back()
+
+end = time.time()
+print("all NTEs recorded in : ",end-start," seconds")
+#################################
