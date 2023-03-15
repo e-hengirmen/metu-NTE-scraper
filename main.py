284a22f47d0046ec23289667dca4dea2ce34d5eb
e-hengirmen <ehengirmen@hotmail.com>
2023-03-15 22:36:51 +0300

    main.py listing bug fixed on (https://muhfd.metu.edu.tr/en/nte-courses) There was 2 problems: 1st was because of table element expiration which we fixed by directly choosing the needed table row on each iteration 2nd was related to selenium's visibility constraint. Fixed it by adding scrolling block and some other minor fixes for

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
+from time import sleep
+
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 start = time.time()
 
+
 def mld_switch(str):
-    dict={
+    dict = {
         "ARAB": "602",
         "FREN": "603",
         "GERM": "604",
@@ -26,12 +32,13 @@ def mld_switch(str):
         "PERS": "612",
         "ENG": "639"
     }
-    courseName=""
+    courseName = ""
     for c in str:
-        if c==' ' or c.isdigit():
+        if c == ' ' or c.isdigit():
             break
-        courseName+=c
+        courseName += c
     return dict[courseName]
+
 
 def write_num(str):
     courseCode = ""
@@ -40,19 +47,22 @@ def write_num(str):
             courseCode += c
     return courseCode
 
-driver = webdriver.Chrome()
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.maximize_window()
 action = ActionChains(driver)
 
 #########################################################
 # change myDEPT to your department
 # delete departments that you dont want to take courses from the class_codes list
-myDEPT='CENG'
-class_codes = [ "120", "121", "125", "230", "232", "233", "236", "240", "241", "310", "311", "312", "314", "410", "420", "450", "453","602", "603", "604", "605", "606", "607", "608","610", "611", "612", "639","642", "643", "644", "651", "682", "831", "863"]
+myDEPT = 'CENG'
+class_codes = ["120", "121", "125", "230", "232", "233", "236", "240", "241", "310", "311", "312", "314", "410", "420",
+               "450", "453", "602", "603", "604", "605", "606", "607", "608", "610", "611", "612", "639", "642", "643",
+               "644", "651", "682", "831", "863"]
 #########################################################
 
 
-NTE_codes=set()
+NTE_codes = set()
 if exists("./NTE_codes.txt"):
     file = open("NTE_codes.txt", "r")
     for course_code in file.readline().split():
@@ -62,13 +72,23 @@ else:
     NTE_URL = "https://muhfd.metu.edu.tr/en/nte-courses"
     driver.get(NTE_URL)
     rows = driver.find_elements(By.XPATH, '//*[@id="content"]/article/div[2]/table/tbody/tr')
-    for i in rows:
-        cols = i.find_elements(By.TAG_NAME, 'td')
+
+    col_td_list=[]
+    for i, index in zip(rows, range(1,1+len(rows))):
+        col_td_list.append(driver.find_elements(By.XPATH, '//*[@id="content"]/article/div[2]/table/tbody/tr['+str(index)+']/td')[0].text)
+    for col_td_text, index in zip(col_td_list, range(1,1+len(col_td_list))):
+        if index<0:
+            continue
         # getting link and course_code
-        class_code = write_num(cols[0].text)
+        class_code = write_num(col_td_text)
         if (int(class_code) >= 603 and int(class_code) <= 639):
             continue
-        i.click()
+        if int(class_code) >311:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.find_elements(By.XPATH, '//*[@id="content"]/article/div[2]/table/tbody/tr['+str(index)+']/td')[0].click()
+        #WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="content"]/article/div[2]/table/tbody/tr['+str(index)+']/td[2]')))[0].click()
+
+        #driver.find_elements(By.XPATH, '//*[@id="content"]/article/div[2]/table/tbody/tr['+str(index)+']/td')[0].click()
 
         if (int(class_code) != 602):
             course_table = WebDriverWait(driver, 10).until(
@@ -87,61 +107,65 @@ else:
                 class_code = mld_switch(course_name)
                 full_code = class_code + ("0" if len(course_code) == 3 else "") + course_code
                 NTE_codes.add(full_code)
-                file.write(full_code+" ")
+                file.write(full_code + " ")
         driver.back()
 
 url = "https://oibs2.metu.edu.tr/View_Program_Course_Details_64/"
 
 #################################
 driver.get(url)
-#<option value="572">Aerospace Engineering/Havacılık ve Uzay Mühendisliği </option>
+# <option value="572">Aerospace Engineering/Havacılık ve Uzay Mühendisliği </option>
 for class_code in class_codes:
-    el2=driver.find_element(By.CSS_SELECTOR,'option[value="'+class_code+'"]')
+    el2 = driver.find_element(By.CSS_SELECTOR, 'option[value="' + class_code + '"]')
     print(el2.text)
     el2.click()
-    driver.find_element(By.XPATH,'//*[@id="single_content"]/form/table[3]/tbody/tr/td/input').click()
+    driver.find_element(By.XPATH, '//*[@id="single_content"]/form/table[3]/tbody/tr/td/input').click()
 
-    #-----------------------fail check---------------
+    # -----------------------fail check---------------
     fm = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "formmessage")))
     # fm = driver.find_elements(By., "There is no")
     if fm.text == "Information about the department could not be found.":
         continue
-    #-----------------------------------------------
+    # -----------------------------------------------
 
-    table=WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,'//*[@id="single_content"]/form/table[4]/tbody/tr')))[1:]
-    for i in range(0,len(table)):
-        row=table[i]
+    table = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.XPATH, '//*[@id="single_content"]/form/table[4]/tbody/tr')))[1:]
+    for i in range(0, len(table)):
+        row = table[i]
         try:
-            column=row.find_element(By.XPATH,"./td[2]")
+            column = row.find_element(By.XPATH, "./td[2]")
         except:
-            table = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,'//*[@id="single_content"]/form/table[4]/tbody/tr')))[1:]
+            table = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.XPATH, '//*[@id="single_content"]/form/table[4]/tbody/tr')))[1:]
             row = table[i]
             column = row.find_element(By.XPATH, "./td[2]")
         if column.text in NTE_codes:
-            rowTEXT=row.text
-            #clicking course
-            row.find_element(By.XPATH,"./td[1]/font/input").click()
-            driver.find_element(By.XPATH,'//*[@id="single_content"]/form/table[2]/tbody/tr/td[1]/input').click()
-            #clicking to sections
-            sections=WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'input[name=submit_section]')))
-            for j in range(0,len(sections)):
-                section=sections[j]
+            rowTEXT = row.text
+            # clicking course
+            row.find_element(By.XPATH, "./td[1]/font/input").click()
+            driver.find_element(By.XPATH, '//*[@id="single_content"]/form/table[2]/tbody/tr/td[1]/input').click()
+            # clicking to sections
+            sections = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'input[name=submit_section]')))
+            for j in range(0, len(sections)):
+                section = sections[j]
                 try:
                     section.click()
                 except:
-                    sections = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'input[name=submit_section]')))
+                    sections = WebDriverWait(driver, 10).until(
+                        EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'input[name=submit_section]')))
                     section = sections[j]
                     section.click()
-                fm = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.ID,"formmessage")))
-                #fm = driver.find_elements(By., "There is no")
-                if fm.text=="There is no section criteria to take the selected courses for this section.":
-                    print("ALL","\t",rowTEXT,j+1)
+                fm = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "formmessage")))
+                # fm = driver.find_elements(By., "There is no")
+                if fm.text == "There is no section criteria to take the selected courses for this section.":
+                    print("ALL", "\t", rowTEXT, j + 1)
 
-                table2=driver.find_elements(By.XPATH,'//*[@id="single_content"]/form/table[3]/tbody/tr')[1:]
+                table2 = driver.find_elements(By.XPATH, '//*[@id="single_content"]/form/table[3]/tbody/tr')[1:]
                 for row2 in table2:
-                    dept=row2.find_element(By.XPATH,'./td[1]').text
-                    if dept=='ALL' or dept==myDEPT:
-                        print(dept,"\t",rowTEXT,j+1)
+                    dept = row2.find_element(By.XPATH, './td[1]').text
+                    if dept == 'ALL' or dept == myDEPT:
+                        print(dept, "\t", rowTEXT, j + 1)
                         break
                 driver.back()
             driver.back()
@@ -150,4 +174,7 @@ for class_code in class_codes:
 driver.quit()
 
 end = time.time()
-print("done ",end-start," seconds")
+print("done ", end - start, " seconds")
+
+
+
