afc1ca1dcd7b0c1d991399e2c46a0813e1fd31f0
ERSEL HENGIRMEN <ehengirmen@hotmail.com>
2023-03-17 19:23:22 +0300

    Added webmanager thus removing a need for chrome webdriver+added requirements.txt

--- capacityCheck.py
+++ capacityCheck.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-import winsound
 from selenium.webdriver.support.ui import WebDriverWait
 
 import os
